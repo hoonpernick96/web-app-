8917f527659f1ee6396f1d28eaf9058e61b71dff
Jaehoon <jaehoon2@illinois.edu>
2022-06-13 14:15:05 +0900

    Add Search UI

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import streamlit as st
+import requests
 
-st.markdown('# first page')
-b = st.button('click')
+url = 'https://floating-harbor-78336.herokuapp.com/fastfood'
 
-if b:
-    st.markdown('''
-    * aaa
-    * bbb
-    ''')
+header = st.container()
+body = st.container()
+
+h_input = header.text_input('패스트푸드 찾기:', placeholder='검색 키워드 입력')
+search_button = header.button('검색')
+
+if search_button:
+    params = {
+        'searchKeyword': h_input
+    }
+
+    response = requests.get(url, params = params).json()
+    body.write(f'총 {response["total"]}개의 패스트푸드점을 찾았습니다.')
+
+    number = 1
+    for i in response['list']:
+        name = i['name']
+        addr = i['addr']
+
+        item_container = body.container()
+        c1, c2 = item_container.columns([1, 7])
+
+        c1.write(number)
+        c2.write(name)
+        c2.write(addr)
+
+        number += 1
