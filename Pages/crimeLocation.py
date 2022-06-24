8362a14cabdecfa9489430527beded6c335b6ab5
Jaehoon <jaehoon2@illinois.edu>
2022-06-24 13:32:18 +0900

    Sort by crime

--- Pages/crimeLocation.py
+++ Pages/crimeLocation.py
@@ -69,12 +69,82 @@ def drawCrimeLocationPage():
     # csv_read.iloc[0:10, 0:2] = 'aaaa'
     # print(csv_read)
 
-    csv_read.set_index('범죄분류', inplace = True)
-    st.dataframe(csv_read, 1000, 1000)
+    # csv_read.set_index('범죄분류', inplace = True)
+    # st.dataframe(csv_read, 1000, 1000)
+    # print(csv_read)
 
     # api = 'https://infuser.odcloud.kr/oas/docs?namespace=15085726/v1'
     # response = requests.get(api).json()
     # st.write(response)
 
+    sort_list = sorted(csv_read.columns)
+    sort_list.remove('범죄분류')
+    choice = st.selectbox('Sort ', sort_list)
+    # df = pd.DataFrame(csv_read)
+    # df = df.sort_values(by=[choice])
+
+    # st.dataframe(df, 1000, 1000)
+
+    header = st.container()
+    body = st.container()
+    footer = st.container()
+
+    col1, col2 = body.columns([10, 7])
+    df = pd.DataFrame(csv_read[['범죄분류', choice]])
+    df = df.sort_values(by=['범죄분류'])
+    df.reset_index(drop=True, inplace=True)
+    col1.dataframe(df, 1000, 7200)
+    # print([csv_read['범죄분류'], csv_read[choice]])
+
+
+
+
+
+
 if __name__ == "__main__":
     drawCrimeLocationPage()
+
+import matplotlib.pyplot as plt
+# plt.plot([1, 3, 7, 4])
+# plt.show()
+
+# plt.plot([1, 3, 7, 4], [1, 4, 9, 16])
+# plt.show()
+
+# 스타일 지정하기
+# plt.plot([1, 3, 7, 4], [1, 4, 9, 16], 'r*-') # red에 *로 표시, r-, r*, ro
+# plt.axis([0, 8, 0, 20]) # x축: 0~8, y축: 0~20
+# plt.show()
+
+# 여러 개의 그래프 그리기
+# import numpy as np
+# t = np.arange(0., 5., 0.2)
+
+#  빨간 대쉬, 파란 사각형, 녹색 삼각형
+# plt.plot(t, t, 'r--', t, t ** 2, 'bs', t, t ** 3, 'g^')
+# plt.show()
+
+# 레이블 패딩
+# plt.plot([1, 2, 3, 4], [2, 3, 5, 10])
+# plt.xlabel('X-Axis', labelpad=15)
+# plt.ylabel('Y-axis', labelpad=20)
+# plt.show()
+
+# front 정하기
+# font1 = {'family': 'serif',
+#          'color': 'b',
+#          'weight': 'bold',
+#          'size': 14
+#          }
+#
+# font2 = {'family': 'fantasy',
+#          'color': 'deeppink',
+#          'weight': 'normal',
+#          'size': 'xx-large'
+#          }
+# plt.plot([1, 2, 3, 4], [2, 3, 5, 10])
+# plt.xlabel('X-axis', labelpad=15, fontdict=font1)
+# plt.xlabel('Y-axis', labelpad=20, fontdict=font2)
+# plt.show()
+
+
